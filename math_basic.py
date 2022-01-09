62a757e5645d1aae556dd4c148d54452e207feb7
Pranali Raorane <pranali02.pr@gmail.com>
2022-01-10 00:25:33 +0530

    Create math_basic.py

---
+++ math_basic.py
@@ -0,0 +1,25 @@
+#Basic Math Programs
+
+#Average of 3 Numbers
+a1= int(input("Enter the first number:"))
+b1= int(input("Enter the second number:"))
+c1= int(input("Enter the third number:"))
+avg= (a1+b1+c1)/3
+print("Average:",avg)
+
+#Square of a Number
+a2= int(input("Enter A Number:"))
+sqr= a2*a2
+print("Square of the Number is",sqr)
+
+#Area Of A Cirlce
+a3= int(input("Enter the Radius of the Circle:"))
+area1= 2*3.14*a3
+print("Area of the Circle is",area1)
+
+#Area Of A Rectangle
+l= int(input("Enter Length of the Rectangle:"))
+b= int(input("Enter Breadth of the Rectangle: "))
+area2= l*b
+print("Area of the Rectangle is", area2)
+
